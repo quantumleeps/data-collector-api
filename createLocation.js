c8dc598213bd7e34026a908c59973c6de1fc6be3
quantumleeps <dan.leeper@gmail.com>
2018-12-07 17:09:23 -0500

    Getting first endpoint available to create location -deployed app again

---
+++ createLocation.js
@@ -0,0 +1,27 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.tableName,
+        Item: {
+            name: data.name,
+            id: uuid.v1(),
+            creatorId: event.requestContext.identity.cognitoIdentityId,
+            country: data.country,
+            description: data.description,
+            equipmentNodeTree: [null],
+            createdAt: Date.now(),
+            modifiedAt: Date.now()
+        }
+    };
+
+    try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+    } catch (e) {
+        return failure({ status: false });
+    }
+}
