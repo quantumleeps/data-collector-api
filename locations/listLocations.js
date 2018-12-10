258cc506e98f03dd7a50eb7f90d2a99cc3623388
quantumleeps <dan.leeper@gmail.com>
2018-12-10 16:54:54 -0500

    Implement the list location that lists by country

--- locations/listLocations.js
+++ locations/listLocations.js
@@ -4,8 +4,15 @@ import { success, failure } from "../libs/response-lib";
 export async function main(event, context) {
   const params = {
     TableName: process.env.locationTableName,
-    Key: {
-      countryId: event.pathParameters.countryId,
+        // 'KeyConditionExpression' defines the condition for the query
+    // - 'userId = :userId': only return items with matching 'userId'
+    //   partition key
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userId': defines 'userId' to be Identity Pool identity id
+    //   of the authenticated user
+    KeyConditionExpression: "countryId = :countryId",
+    ExpressionAttributeValues: {
+      ":countryId": event.pathParameters.countryId
     }
   };
 
